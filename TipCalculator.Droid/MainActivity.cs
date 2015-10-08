3fe6dd7b10600cf275b189c907c8afdbd1c2f899
CRE <christoph.rehmann@gmail.com>
2015-10-08 22:32:23 +0200

    Example of binding a jar library with a public class the extends a non-public class

--- TipCalculator.Droid/MainActivity.cs
+++ TipCalculator.Droid/MainActivity.cs
@@ -1,14 +1,22 @@
-﻿using System;
-using Android.App;
-using Android.Widget;
+﻿using Android.App;
 using Android.OS;
+using Android.Widget;
+using Crehmann.Samples.Monodroidjarexample;
+using System;
 
 namespace TipCalculator.Droid
 {
     [Activity(Label = "TipCalculator.Droid", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        private readonly ITipCalculationService _tipService;
         private TextView _tipOutput;
+        private EditText _priceEditTExt;
+
+        public MainActivity()
+        {
+            _tipService = new TipCalculationServiceImpl();
+        }
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -17,17 +25,28 @@ namespace TipCalculator.Droid
             InitializeView();
         }
 
+
         private void InitializeView()
         {
             _tipOutput = FindViewById<TextView>(Resource.Id.textview_tip_output);
+            _priceEditTExt = FindViewById<EditText>(Resource.Id.edittext_tip_input);
             var calculateButton = FindViewById<Button>(Resource.Id.button_calulate);
             calculateButton.Click += CalculateButton_Click;
         }
 
+
         private void CalculateButton_Click(object sender, EventArgs e)
         {
+            double price;
+
+            if(double.TryParse(_priceEditTExt.Text, out price))
+            {
+                var tipRequest = new TipRequest() { Price = price, Satisfaction = Satisfaction.Excellent };
+                var result = _tipService.CalculateTip(tipRequest);
+                _tipOutput.Text = result.Tip.ToString("0.00");
+            }
+            
             
         }
     }
 }
-
--- TipCalculator.Droid/Resources/Resource.Designer.cs
+++ TipCalculator.Droid/Resources/Resource.Designer.cs
@@ -60,14 +60,14 @@ namespace TipCalculator.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050001
-			public const int button_calulate = 2131034113;
+			// aapt resource value: 0x7f050002
+			public const int button_calulate = 2131034114;
 			
 			// aapt resource value: 0x7f050000
 			public const int edittext_tip_input = 2131034112;
 			
-			// aapt resource value: 0x7f050002
-			public const int textview_tip_output = 2131034114;
+			// aapt resource value: 0x7f050001
+			public const int textview_tip_output = 2131034113;
 			
 			static Id()
 			{
